e84bb97b35bc47b8f910fe5d41789e7c999a8627
Julija Katukova <julija.katukova@gmail.com>
2021-06-30 19:10:36 +0300

    additional solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,5 +18,47 @@ public class Main {
             }
         }
         System.out.println("There are " + count + " vowels in the string, " + line);
+
+        // Alternative solution
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        String apple = "watermelon";
+        // 1. change apple to char array
+        char[] appleArray = apple.toCharArray();
+        int vowelCounter = 0;
+        for (int i = 0; i < appleArray.length; i++) {
+            for (int j = 0; j < vowels.length; j++) {
+                if(appleArray[i] == vowels[j]) {
+                    vowelCounter++;
+                }
+            }
+        }
+        System.out.println("The number of vowels in " + apple + " is " + vowelCounter);
+
+        // 2. loop through apple
+        vowelCounter = 0;
+        for (int i = 0; i < apple.length(); i++) {
+            for (int j = 0; j < vowels.length; j++) {
+                if(apple.charAt(i) == vowels[j]) {
+                    vowelCounter++;
+                }
+            }
+        }
+        System.out.println("The number of vowels in " + apple + " is " + vowelCounter);
+
+        //switch
+        vowelCounter = 0;
+        for (int i = 0; i < apple.length(); i++) {
+            switch (apple.charAt(i)) {
+                case 'a':
+                case 'i':
+                case 'e':
+                case 'o':
+                case 'u':
+                    vowelCounter++;
+                    break;
+            }
+
+        }
+        System.out.println("The number of vowels in " + apple + " is " + vowelCounter);
     }
 }
